64bdb53c2f783da865f97790d8aca39a5b0de11a
AkashMondal27 <akashmondal102003@gmail.com>
2026-01-22 22:16:09 +0530

    Initial portfolio files

---
+++ script.js
@@ -0,0 +1,78 @@
+//******************** NAVBAR SECTION ********************** */
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+const overlay = document.getElementById("menuOverlay");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  overlay.classList.toggle("active");
+
+  // lock body scroll
+  document.body.style.overflow =
+    navLinks.classList.contains("active") ? "hidden" : "auto";
+});
+
+// Close menu when clicking overlay
+overlay.addEventListener("click", () => {
+  navLinks.classList.remove("active");
+  overlay.classList.remove("active");
+  document.body.style.overflow = "auto";
+});
+
+// Optional: close on link click
+document.querySelectorAll(".nav-links a").forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+    overlay.classList.remove("active");
+    document.body.style.overflow = "auto";
+  });
+});
+
+
+const names = [
+  "Akash Mondal",        // English
+  "आकाश मंडल",          // Hindi
+  "আকাশ মন্ডল",         // Bengali
+ 
+
+];
+
+const nameEl = document.getElementById("nameRotate");
+let index = 0;
+
+setInterval(() => {
+  nameEl.classList.add("fade-out");
+
+  setTimeout(() => {
+    index = (index + 1) % names.length;
+    nameEl.textContent = names[index];
+    nameEl.classList.remove("fade-out");
+    nameEl.classList.add("fade-in");
+  }, 400);
+
+}, 2500);
+
+//******************** CONTACT FORM SECTION ********************** */
+
+//pop up notification for contact form submission
+const form = document.getElementById("contactForm");
+const popup = document.getElementById("popup");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    const response = await fetch(form.action, {
+        method: "POST",
+        body: data,
+        headers: { "Accept": "application/json" }
+    });
+
+    if(response.ok){
+        popup.classList.add("show");
+        setTimeout(() => popup.classList.remove("show"), 2500);
+        form.reset();
+    }
+});
